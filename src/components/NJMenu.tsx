16841bfb0af47f8e651d2c19b72d2a509779b251
Niranjan Devasani <niranjan.devasani@gmail.com>
2023-04-12 00:02:57 +0530

    Some fixes

--- src/components/NJMenu.tsx
+++ src/components/NJMenu.tsx
@@ -21,7 +21,7 @@ export const NJMenu = (props: Props) => {
         leave="transition ease-in duration-75"
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95">
-        <Menu.Items className="absolute right-0 mt-0 w-[150px] origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 mt-1 w-[150px] origin-top-right divide-y divide-gray-100 rounded card-background shadow-lg">
           {children}
         </Menu.Items>
       </Transition>
--- src/examples/MenuExample.tsx
+++ src/examples/MenuExample.tsx
@@ -5,16 +5,14 @@ const MenuExample = () => {
   return (
     <NJMenu
       trigger={
-        <button
-          type="button"
-          className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+        <button type="button" className="rounded card-background px-4 py-2 text-sm font-medium text border">
           Open Menu
         </button>
       }>
       <div className="p-1">
         <Menu.Item>
           {({ active }) => (
-            <button className={`${active ? 'bg-violet-500 text-white' : ''} text-gray-900 flex w-full items-center rounded-md px-2 py-2 text-sm`}>
+            <button className={`${active ? 'background-primary text-white' : ''} text flex w-full items-center rounded px-2 py-2 text-sm`}>
               Edit
             </button>
           )}
