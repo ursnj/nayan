71c3465d2405d85dc16b001682b25519c3711093
Niranjan Devasani <niranjan.devasani@gmail.com>
2023-04-12 02:03:15 +0530

    Some fixes

--- src/components/NJSelect.tsx
+++ src/components/NJSelect.tsx
@@ -1,27 +1,48 @@
 import { Listbox, Transition } from '@headlessui/react';
-import { Fragment, ReactNode } from 'react';
-import { ChevronUpDownIcon } from './Icons';
+import { Fragment } from 'react';
+import { CheckIcon, ChevronUpDownIcon } from './Icons';
 
 interface Props {
-  children: ReactNode;
+  items: any;
   selected: any;
   onSelected: (val: any) => void;
 }
 
 export const NJSelect = (props: Props) => {
-  const { selected, children } = props;
+  const { selected, items } = props;
   return (
     <Listbox value={selected} onChange={props.onSelected} multiple={Array.isArray(selected)}>
       <div className="relative">
-        <Listbox.Button className="relative w-full cursor-default rounded card-background border py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-          <span className="block truncate text">{Array.isArray(selected) ? selected.length + ' selected' : selected.name}</span>
+        <Listbox.Button className="relative w-full cursor-pointer rounded card-background border py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+          <span className="block truncate text">{Array.isArray(selected) ? selected.length + ' selected' : selected.label}</span>
           <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
             <ChevronUpDownIcon className="h-5 w-5 text-muted" aria-hidden="true" />
           </span>
         </Listbox.Button>
         <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
           <Listbox.Options className="absolute mt-1 max-h-60 w-full z-10 overflow-auto rounded card-background border py-1 text-base shadow-lg">
-            {children}
+            {items.map((item: any) => (
+              <Listbox.Option
+                key={item.id}
+                value={item}
+                disabled={!!item.disable}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 pl-10 pr-4 ${!!item.disable ? 'text-muted cursor-not-allowed' : 'text cursor-pointer'} ${
+                    active ? 'background-muted' : ''
+                  }`
+                }>
+                {({ selected }) => (
+                  <>
+                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>{item.label}</span>
+                    {selected ? (
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-primary">
+                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    ) : null}
+                  </>
+                )}
+              </Listbox.Option>
+            ))}
           </Listbox.Options>
         </Transition>
       </div>
--- src/examples/SelectExample.tsx
+++ src/examples/SelectExample.tsx
@@ -1,64 +1,24 @@
-import { Listbox } from '@headlessui/react';
-import { CheckIcon } from '../components/Icons';
 import { NJSelect } from '../components/NJSelect';
 import { useState } from 'react';
 
-const people = [
-  { name: 'Wade Cooper' },
-  { name: 'Arlene Mccoy' },
-  { name: 'Devon Webb' },
-  { name: 'Tom Cook' },
-  { name: 'Tanya Fox' },
-  { name: 'Hellen Schmidt' }
+const items = [
+  { id: '1', label: 'Wade Cooper' },
+  { id: '2', label: 'Arlene Mccoy' },
+  { id: '3', label: 'Devon Webb' },
+  { id: '4', label: 'Tom Cook' },
+  { id: '5', label: 'Tanya Fox' },
+  { id: '6', label: 'Hellen Schmidt', disable: true }
 ];
 
 const SelectExample = () => {
-  const [selected, setSelected] = useState(people[0]);
-  const [selected1, setSelected1] = useState([people[0]]);
+  const [selected, setSelected] = useState(items[0]);
+  const [selected1, setSelected1] = useState([items[0]]);
 
   return (
     <div>
-      <NJSelect selected={selected} onSelected={setSelected}>
-        {people.map((person, personIdx) => (
-          <Listbox.Option
-            key={personIdx}
-            className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 text ${active ? 'background-muted' : ''}`}
-            value={person}>
-            {({ selected }) => (
-              <>
-                <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>{person.name}</span>
-                {selected ? (
-                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-primary">
-                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                  </span>
-                ) : null}
-              </>
-            )}
-          </Listbox.Option>
-        ))}
-      </NJSelect>
-
+      <NJSelect items={items} selected={selected} onSelected={setSelected} />
       <div className="mt-3" />
-
-      <NJSelect selected={selected1} onSelected={setSelected1}>
-        {people.map((person, personIdx) => (
-          <Listbox.Option
-            key={personIdx}
-            className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 text ${active ? 'card-background' : ''}`}
-            value={person}>
-            {({ selected }) => (
-              <>
-                <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>{person.name}</span>
-                {selected ? (
-                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-primary">
-                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                  </span>
-                ) : null}
-              </>
-            )}
-          </Listbox.Option>
-        ))}
-      </NJSelect>
+      <NJSelect items={items} selected={selected1} onSelected={setSelected1} />
     </div>
   );
 };
