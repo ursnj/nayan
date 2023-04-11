6a1cef29cf0518e5cfc20dda4eb7feb39ade9823
ndevasan <ndevasan@cisco.com>
2023-04-11 16:38:13 +0530

    Some Fixes

--- src/components/custom-popover/CustomPopover.tsx
+++ src/components/custom-popover/CustomPopover.tsx
@@ -9,26 +9,22 @@ interface Props {
 const CustomPopover = (props: Props) => {
   return (
     <Popover className="relative">
-      {({ open }) => (
-        <>
-          <Popover.Button>
-            {props.trigger}
-          </Popover.Button>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
-          >
-            <Popover.Panel className="absolute right-0 z-10 mt-2 rounded w-screen max-w-[250px] -translate-x-0 transform">
-              {props.children}
-            </Popover.Panel>
-          </Transition>
-        </>
-      )}
+      <Popover.Button>
+        {props.trigger}
+      </Popover.Button>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-200"
+        enterFrom="opacity-0 translate-y-1"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition ease-in duration-150"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-1"
+      >
+        <Popover.Panel className="absolute right-0 z-10 mt-2 rounded w-screen max-w-[250px] -translate-x-0 transform">
+          {props.children}
+        </Popover.Panel>
+      </Transition>
     </Popover>
   );
 };
