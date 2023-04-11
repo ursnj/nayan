bc15ba9fbc67339d49d8a202ac5c59d13081271c
Niranjan Devasani <niranjan.devasani@gmail.com>
2023-04-12 00:35:56 +0530

    Some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -17,28 +17,30 @@ const App = () => {
 
   return (
     <div className="w-screen h-screen p-3">
-      <div className="mt-3">
-        <button className="text-primary" onClick={() => localStorage.setItem('THEME', theme === 'light' ? 'dark' : 'light')}>
-          Toggle Theme
-        </button>
-      </div>
-      <div className="mt-3">
-        <SelectExample />
-      </div>
-      <div className="mt-3 text-right">
-        <MenuExample />
-      </div>
-      <div className="mt-3">
-        <DialogExample />
-      </div>
-      <div className="mt-3">
-        <SwitchExample />
-      </div>
-      <div className="mt-3">
-        <DisClosureExample />
-      </div>
-      <div className="mt-3 text-right">
-        <PopoverExample />
+      <div className="max-w-lg m-auto">
+        <div className="mt-3">
+          <button className="text-primary" onClick={() => localStorage.setItem('THEME', theme === 'light' ? 'dark' : 'light')}>
+            Toggle Theme
+          </button>
+        </div>
+        <div className="mt-3">
+          <SelectExample />
+        </div>
+        <div className="mt-3 text-right">
+          <MenuExample />
+        </div>
+        <div className="mt-3 text-right">
+          <PopoverExample />
+        </div>
+        <div className="mt-3 text-right">
+          <DialogExample />
+        </div>
+        <div className="mt-3">
+          <SwitchExample />
+        </div>
+        <div className="mt-3">
+          <DisClosureExample />
+        </div>
       </div>
     </div>
   );
