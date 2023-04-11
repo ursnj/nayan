ab533f1e64e29c580bfed3a6b70f3f4f5952bb4d
Niranjan Devasani <niranjan.devasani@gmail.com>
2023-04-12 00:14:31 +0530

    Some fixes

--- src/components/NJSelect.tsx
+++ src/components/NJSelect.tsx
@@ -13,10 +13,10 @@ export const NJSelect = (props: Props) => {
   return (
     <Listbox value={selected} onChange={props.onSelected} multiple={Array.isArray(selected)}>
       <div className="relative">
-        <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-          <span className="block truncate">{Array.isArray(selected) ? selected.length + ' selected' : selected.name}</span>
+        <Listbox.Button className="relative w-full cursor-default rounded card-background border py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+          <span className="block truncate text">{Array.isArray(selected) ? selected.length + ' selected' : selected.name}</span>
           <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-            <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+            <ChevronUpDownIcon className="h-5 w-5 text-muted" aria-hidden="true" />
           </span>
         </Listbox.Button>
         <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
