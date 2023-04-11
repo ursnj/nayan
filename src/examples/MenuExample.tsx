bff1f85a51d58402266b818a5c84415e038a5889
Niranjan Devasani <niranjan.devasani@gmail.com>
2023-04-12 00:04:52 +0530

    Some fixes

--- src/examples/MenuExample.tsx
+++ src/examples/MenuExample.tsx
@@ -11,11 +11,7 @@ const MenuExample = () => {
       }>
       <div className="p-1">
         <Menu.Item>
-          {({ active }) => (
-            <button className={`${active ? 'background-primary text-white' : ''} text flex w-full items-center rounded px-2 py-2 text-sm`}>
-              Edit
-            </button>
-          )}
+          <button className="text flex w-full items-center rounded px-2 py-2 text-sm">Edit</button>
         </Menu.Item>
       </div>
     </NJMenu>
