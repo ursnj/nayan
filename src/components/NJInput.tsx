a29f7d0ef1d682f9c803e08d5528946fd28237b3
ndevasan <ndevasan@cisco.com>
2023-04-12 19:31:03 +0530

    Some fixes

--- src/components/NJInput.tsx
+++ src/components/NJInput.tsx
@@ -1,15 +1,26 @@
 interface Props {
   type?: string;
   label?: string;
+  value?: string;
+  defaultValue?: string;
   placeholder?: string;
+  onChange?: (val: any) => void;
 }
 
 export const NJInput = (props: Props) => {
-  const { type = 'text', label = '', placeholder = '' } = props;
+  const { type = 'text', label = '', placeholder = '', value = '', defaultValue = '', onChange, ...remaining } = props;
   return (
     <div className="mb-3">
       {label && <label className="block pb-1 text-muted">{label}</label>}
-      <input type={type} placeholder={placeholder} className="w-full rounded card-background border text px-3 py-2" />
+      <input
+        type={type}
+        {...(value && { value })}
+        {...(defaultValue && { defaultValue })}
+        {...(placeholder && { placeholder })}
+        {...(onChange && { onChange })}
+        {...remaining}
+        className="w-full rounded card-background border text px-3 py-2"
+      />
     </div>
   );
 };
--- src/examples/InputExample.tsx
+++ src/examples/InputExample.tsx
@@ -1,10 +1,21 @@
 import { NJInput } from '../components/NJInput';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const InputExample = () => {
+  const { register, handleSubmit } = useForm();
+  const [email, setEmail] = useState('niranjan.devasani@gmail.com');
+
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <>
-      <h1 className="text mb-3 text-lg text-left">Input:</h1>
-      <NJInput type="email" label="Email" placeholder="Enter email" />
+      <h1 className="text mb-3 text-lg text-left">Custom Input:</h1>
+      <NJInput type="email" label="Email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1 className="text mb-3 mt-3 text-lg text-left">React Hook Form Input:</h1>
+        <NJInput type="email" label="Email" placeholder="Enter email" defaultValue="niranjan.devasani@gmail.com" {...register('email')} />
+      </form>
     </>
   );
 };
