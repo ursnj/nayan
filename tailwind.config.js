c34f1df2f3cf50f7c77a329bbd0da895f730c659
ndevasan <ndevasan@cisco.com>
2023-04-13 15:19:49 +0530

    Some fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        'primary': '#005ee6',
+        primary: '#005ee6',
         'primary-dark': '#0043a3',
         'primary-light': '#0069ff'
       }
