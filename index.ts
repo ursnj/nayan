d21b21c1fe6149be8961c1f1f952b3863cae0764
Niranjan Devasani <niranjan.devasani@gmail.com>
2023-04-15 01:05:50 +0530

    Some fixes

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ export * from './src/components/NDialog';
 export * from './src/components/NDivider';
 export * from './src/components/NDisclosure';
 export * from './src/components/NInput';
+export * from './src/components/NLink';
 export * from './src/components/NMenu';
 export * from './src/components/NMenuItem';
 export * from './src/components/NPopover';
