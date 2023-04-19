da992deb1650b7cf89bc8834451a88e2a289dba2
Niranjan Devasani <niranjan.devasani@gmail.com>
2023-04-19 14:14:39 +0530

    release now

--- src/components/NDivider.tsx
+++ src/components/NDivider.tsx
@@ -6,7 +6,7 @@ interface Props {
 export const NDivider = (props: Props) => {
   const { className = '', text = '' } = props;
   return (
-    <div className={`nyn-divider relative flex py-5 items-center ${className}`}>
+    <div className={`nyn-divider relative flex items-center ${className}`}>
       <div className="flex-grow nyn-border-top"></div>
       {text && <span className="nyn-divider-text nyn-text-muted flex-shrink mx-3">{text}</span>}
       <div className="flex-grow nyn-border-top"></div>
