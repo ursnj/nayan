8e231960a493be6740210bfaeba6299b73866ff7
Niranjan Devasani <niranjan.devasani@gmail.com>
2023-05-05 07:18:11 +0530

    release now

--- README.md
+++ README.md
@@ -334,17 +334,15 @@ const DialogExample = () => {
   return (
     <div className="break-inside-avoid mb-16 flex flex-row justify-between">
       <h1 className="nyn-text mb-3 text-lg text-left">Dialog:</h1>
+      <NButton size={Size.SM} className="text-sm font-medium text-white bg-green-500 hover:bg-green-600" onClick={() => setIsOpen(true)}>
+        Open Dialog
+      </NButton>
       <NDialog
         size={Size.SM}
         isOpen={isOpen}
         isDismissable={true}
         title="Payment confirmation"
-        closeModal={() => setIsOpen(false)}
-        trigger={
-          <NButton className="text-sm font-medium text-white bg-green-500 hover:bg-green-600" onClick={() => setIsOpen(true)}>
-            Open Dialog
-          </NButton>
-        }>
+        closeModal={() => setIsOpen(false)}>
         <div className="p-3 w-full h-full">
           <p className="text-sm nyn-text">
             Your payment has been successfully submitted. We’ve sent you an email with all of the details of your order.
--- src/components/NDialog.tsx
+++ src/components/NDialog.tsx
@@ -8,7 +8,6 @@ interface Props {
   title: string;
   isOpen: boolean;
   isDismissable?: boolean;
-  trigger: ReactNode;
   children: ReactNode;
   closeModal: () => void;
 }
@@ -21,50 +20,47 @@ const sizeMapping = {
 };
 
 export const NDialog = (props: Props) => {
-  const { title, trigger, children, isOpen, closeModal, size = Size.SM, isDismissable = true } = props;
+  const { title, children, isOpen, closeModal, size = Size.SM, isDismissable = true } = props;
   return (
-    <>
-      <div className="">{trigger}</div>
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={isDismissable ? closeModal : () => undefined}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0">
-            <div className="fixed inset-0 nyn-background-overlay" />
-          </Transition.Child>
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={isDismissable ? closeModal : () => undefined}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0">
+          <div className="fixed inset-0 nyn-background-overlay" />
+        </Transition.Child>
 
-          <div className="nyn-dialog fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-3 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95">
-                <Dialog.Panel
-                  className={`nyn-dialog-panel w-full ${sizeMapping[size]} transform overflow-hidden rounded nyn-background-card nyn-border text-left align-middle transition-all shadow-lg`}>
-                  <div className="flex flex-row justify-between items-center py-2 px-3">
-                    <Dialog.Title as="div" className="nyn-dialog-title text-base font-medium leading-6 nyn-text">
-                      {title}
-                    </Dialog.Title>
-                    <span className="cursor-pointer" onClick={closeModal}>
-                      <CloseIcon className="w-5 h-5 nyn-text" />
-                    </span>
-                  </div>
-                  <div className="nyn-dialog-content w-full h-full min-h-100 max-h-600">{children}</div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
+        <div className="nyn-dialog fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-3 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95">
+              <Dialog.Panel
+                className={`nyn-dialog-panel w-full ${sizeMapping[size]} transform overflow-hidden rounded nyn-background-card nyn-border text-left align-middle transition-all shadow-lg`}>
+                <div className="flex flex-row justify-between items-center py-2 px-3">
+                  <Dialog.Title as="div" className="nyn-dialog-title text-base font-medium leading-6 nyn-text">
+                    {title}
+                  </Dialog.Title>
+                  <span className="cursor-pointer" onClick={closeModal}>
+                    <CloseIcon className="w-5 h-5 nyn-text" />
+                  </span>
+                </div>
+                <div className="nyn-dialog-content w-full h-full min-h-100 max-h-600">{children}</div>
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
-        </Dialog>
-      </Transition>
-    </>
+        </div>
+      </Dialog>
+    </Transition>
   );
 };
