09cec3772dfb675d05b376f892896924291cba74
ndevasan <ndevasan@cisco.com>
2023-07-07 20:45:44 +0530

    Some fixes

--- vite.website.ts
+++ vite.website.ts
@@ -1,6 +1,7 @@
 /** @type {import('vite').UserConfig} */
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,5 +23,10 @@ export default defineConfig({
         global: 'globalThis'
       }
     }
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
