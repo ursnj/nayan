c8702ae9da5d796449e15a9ad68597012dfd9d86
ndevasan <ndevasan@cisco.com>
2023-07-08 12:02:34 +0530

    Some fixes

--- src/examples/ConfirmAlertExample.tsx
+++ src/examples/ConfirmAlertExample.tsx
@@ -8,7 +8,7 @@ const ConfirmAlertExample = () => {
         title="Are you absolutely sure?"
         message="This action cannot be undone. This will permanently delete your account and remove your data from our servers."
         trigger={<div className="text-sm font-medium text-pink-600">Confirm Alert</div>}
-        onConfirmClick={() => console.log('Confirm CLicked')}
+        onConfirmClick={() => console.log('Confirm Clicked')}
         onCancelClick={() => console.log('Cancel clicked')}
       />
     </div>
