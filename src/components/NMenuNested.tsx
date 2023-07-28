ae9949407500a05e791724c559edea0c83293b62
ndevasan <ndevasan@cisco.com>
2023-07-28 14:35:02 +0530

    Some fixes

--- src/components/NMenuNested.tsx
+++ src/components/NMenuNested.tsx
@@ -14,7 +14,7 @@ export const NMenuNested = (props: Props) => {
   const { trigger, children, className = '', size = Size.MD } = props;
   return (
     <DropdownMenuSub>
-      <DropdownMenuSubTrigger className={`nyn-menu-nested-trigger text-muted ${className}`}>{trigger}</DropdownMenuSubTrigger>
+      <DropdownMenuSubTrigger className={`nyn-menu-nested-trigger text-muted hover:bg-border ${className}`}>{trigger}</DropdownMenuSubTrigger>
       <DropdownMenuPortal>
         <DropdownMenuSubContent className={`nyn-menu-nested-content rounded bg-card border border-border shadow-lg ${menuSizeMapping[size]}`}>
           {children}
