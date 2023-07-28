2de5b73f7dcaa95ee896ead9a6d3b0010ca91792
ndevasan <ndevasan@cisco.com>
2023-07-28 14:31:36 +0530

    Some fixes

--- src/components/NButtonGroup.tsx
+++ src/components/NButtonGroup.tsx
@@ -17,8 +17,8 @@ export const NButtonGroup = (props: Props) => {
           type="button"
           onClick={() => setSelected(item)}
           className={`nyn-button-group ${
-            item === selected ? 'bg-primary' : 'bg-card'
-          } text-text hover:bg-primary-dark hover:text-white border border-border first:border-r-0 last:border-l-0 first:rounded-l-lg rounded-none last:rounded-r-lg px-3 py-1 mx-0 outline-none focus:shadow-outline ${className}`}>
+            item === selected ? 'bg-primary text-white' : 'bg-card text-text'
+          } hover:bg-primary-dark hover:text-white border border-border first:border-r-0 last:border-l-0 first:rounded-l-lg rounded-none last:rounded-r-lg px-3 py-2 h-auto mx-0 outline-none focus:shadow-outline ${className}`}>
           {item}
         </Button>
       ))}
--- src/components/NMenuItem.tsx
+++ src/components/NMenuItem.tsx
@@ -16,10 +16,10 @@ export const NMenuItem = (props: Props) => {
 
   return (
     <>
-      <DropdownMenuItem className={`nyn-menu-item text-text hover:bg-border cursor-pointer ${className}`} onClick={props.onClick}>
+      <DropdownMenuItem className={`nyn-menu-item text-text hover:bg-border cursor-pointer py-2 ${className}`} onClick={props.onClick}>
         {props.icon && <Icon className="mr-2 h-4 w-4" />}
         <span>{title}</span>
-        {shortcut && <DropdownMenuShortcut className="nyn-text-muted">⇧⌘P</DropdownMenuShortcut>}
+        {shortcut && <DropdownMenuShortcut className="text-muted">⇧⌘P</DropdownMenuShortcut>}
       </DropdownMenuItem>
       {separator && <NDivider />}
     </>
