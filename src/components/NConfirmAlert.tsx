d64de06d11ab5bd051a34edff73b978baa20a085
ndevasan <ndevasan@cisco.com>
2023-07-29 22:38:06 +0530

    Some fixes

--- src/components/NConfirmAlert.tsx
+++ src/components/NConfirmAlert.tsx
@@ -21,13 +21,13 @@ interface Props {
 }
 
 export const NConfirmAlert = (props: Props) => {
-  const { isOpen, title, message, className = '', confirmText = 'Confirm', cancelText = 'Cancel' } = props;
+  const { isOpen, title = 'Are you absolutely sure?', message, className = '', confirmText = 'Confirm', cancelText = 'Cancel' } = props;
 
   return (
     <AlertDialog open={isOpen} onOpenChange={props.onClose}>
       <AlertDialogContent className={`nyn-confirm-alert border border-border bg-card p-3 ${className}`}>
         <AlertDialogHeader>
-          {title && <AlertDialogTitle className="nyn-confirm-alert-title text-text">{title}</AlertDialogTitle>}
+          <AlertDialogTitle className="nyn-confirm-alert-title text-text">{title}</AlertDialogTitle>
           <AlertDialogDescription className="nyn-confirm-alert-message text-text">{message}</AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
