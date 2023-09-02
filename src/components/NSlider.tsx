8dca705fe056de88e42a5820a9f1dd49c927111b
Niranjan Devasani <niranjan.devasani@gmail.com>
2023-09-02 18:50:50 +0530

    Add Slider

--- src/components/NSlider.tsx
+++ src/components/NSlider.tsx
@@ -6,11 +6,19 @@ interface Props {
   max?: number;
   step?: number;
   isDisabled?: boolean;
+  onChange: (value: number) => void;
 }
 
 export const NSlider = (props: Props) => {
   const { className = '', defaultValue = 0, max = 100, step = 1, isDisabled = false } = props;
   return (
-    <Slider defaultValue={[defaultValue]} max={max} step={step} disabled={isDisabled} className={`nyn-slider bg-border rounded  ${className}`} />
+    <Slider
+      defaultValue={[defaultValue]}
+      max={max}
+      step={step}
+      disabled={isDisabled}
+      onValueChange={value => props.onChange(value[0])}
+      className={`nyn-slider bg-border rounded  ${className}`}
+    />
   );
 };
