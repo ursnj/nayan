4a42ddea47442fe0c1439de27419e3dfbe3226f5
Niranjan Devasani <niranjan.devasani@gmail.com>
2023-09-03 09:25:01 +0530

    Add Slider

--- src/components/NMenuItem.tsx
+++ src/components/NMenuItem.tsx
@@ -16,7 +16,9 @@ export const NMenuItem = (props: Props) => {
 
   return (
     <>
-      <DropdownMenuItem className={`nyn-menu-item text-text hover:bg-border cursor-pointer py-2 ${className}`} onClick={props.onClick}>
+      <DropdownMenuItem
+        className={`nyn-menu-item text-text hover:bg-border focus:bg-border active:bg-border cursor-pointer py-2 ${className}`}
+        onClick={props.onClick}>
         {props.icon && <Icon className="mr-2 h-4 w-4" />}
         <span>{title}</span>
         {shortcut && <DropdownMenuShortcut className="text-muted">⇧⌘P</DropdownMenuShortcut>}
--- src/components/NMenuNested.tsx
+++ src/components/NMenuNested.tsx
@@ -14,7 +14,9 @@ export const NMenuNested = (props: Props) => {
   const { trigger, children, className = '', size = Size.MD } = props;
   return (
     <DropdownMenuSub>
-      <DropdownMenuSubTrigger className="nyn-menu-nested-trigger text-muted hover:bg-border py-2.5">{trigger}</DropdownMenuSubTrigger>
+      <DropdownMenuSubTrigger className="nyn-menu-nested-trigger text-muted hover:bg-border focus:bg-border active:bg-border py-2.5">
+        {trigger}
+      </DropdownMenuSubTrigger>
       <DropdownMenuPortal>
         <DropdownMenuSubContent
           className={`nyn-menu-nested-content rounded bg-card border border-border shadow-lg ${menuSizeMapping[size]} ${className}`}>
