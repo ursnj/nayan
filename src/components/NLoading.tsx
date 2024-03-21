74faec4125f4e213282dc7f33aa7ee679a0ab9f9
ndevasan <ndevasan@cisco.com>
2024-03-21 16:05:51 +0530

    Simplify setup

--- src/components/NLoading.tsx
+++ src/components/NLoading.tsx
@@ -1,3 +1,5 @@
+import './NStyles.css';
+
 interface Props {
   className?: string;
 }
@@ -6,7 +8,7 @@ export const NLoading = (props: Props) => {
   return (
     <div className="p-3">
       <div
-        className={`animate-loader relative text-slate-400 text-[5px] h-[1em] w-[1em] my-[20px] mx-auto -indent-[999em] rounded-full ${props.className}`}>
+        className={`animation-loader relative text-slate-400 text-[5px] h-[1em] w-[1em] my-[20px] mx-auto -indent-[999em] rounded-full ${props.className}`}>
         Loading...
       </div>
     </div>
