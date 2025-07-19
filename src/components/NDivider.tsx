22485da75c7fc7b698b1ad1ea354956c5c8357a0
ursnj <niranjan.devasani@gmail.com>
2025-07-20 00:10:16 +0530

    Improve NDivider Component

--- src/components/NDivider.tsx
+++ src/components/NDivider.tsx
@@ -5,23 +5,24 @@ import { cn } from '@/lib/utils';
 export interface NDividerProps extends React.ComponentProps<typeof Separator> {
   children?: ReactNode;
   childrenClassName?: string;
+  separatorClassName?: string;
 }
 
 const NDividerComponent: React.FC<NDividerProps> = memo(
-  ({ className = '', orientation = 'horizontal', children, childrenClassName = '', ...rest }) => {
+  ({ className = '', orientation = 'horizontal', children, childrenClassName = '', separatorClassName = '', ...rest }) => {
     if (children && orientation === 'vertical') {
-      return <Separator orientation={orientation} className={cn('bg-border', className)} {...rest} />;
+      return <Separator orientation={orientation} className={cn('bg-border', separatorClassName)} {...rest} />;
     }
     if (children && orientation === 'horizontal') {
       return (
         <div className={cn('flex items-center', className)}>
-          <Separator orientation="horizontal" className={cn('flex-1 h-px', className)} {...rest} />
+          <Separator orientation="horizontal" className={cn('flex-1 h-px', separatorClassName)} {...rest} />
           <span className={cn('mx-2 text-border text-xs whitespace-nowrap', childrenClassName)}>{children}</span>
-          <Separator orientation="horizontal" className={cn('flex-1 h-px', className)} {...rest} />
+          <Separator orientation="horizontal" className={cn('flex-1 h-px', separatorClassName)} {...rest} />
         </div>
       );
     }
-    return <Separator orientation={orientation} className={cn('bg-border', className)} {...rest} />;
+    return <Separator orientation={orientation} className={cn('bg-border', separatorClassName || className)} {...rest} />;
   }
 );
 
