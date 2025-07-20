6f8a45282189e0d9a100d04c7373f6e1da132149
ursnj <niranjan.devasani@gmail.com>
2025-07-20 20:09:51 +0530

    Add Monorepo

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,34 +1,29 @@
-import path from 'path';
-import { defineConfig } from 'vite';
+/** @type {import('vite').UserConfig} */
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   server: {
     host: 'localhost',
     port: 3000,
     open: true
   },
   build: {
-    lib: {
-      entry: './src/index.ts',
-      name: 'Nayan',
-      fileName: format => `index.${format}.js`
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    },
-    sourcemap: true,
-    emptyOutDir: true,
-    copyPublicDir: false
+    outDir: 'build',
+    sourcemap: true
   },
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
