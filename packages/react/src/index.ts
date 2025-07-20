ac6852d3a0d5fba8c860cfc68f41be375606e8ca
ursnj <niranjan.devasani@gmail.com>
2025-07-21 01:26:14 +0530

    Add Monorepo

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -31,36 +31,9 @@ export * from './components/NTabs';
 export * from './components/NTabsContent';
 export * from './components/NTextarea';
 export * from './components/NTheme';
-export { useToast as useNToast, showToast } from './components/NToast';
+export * from './components/NToast';
 export * from './components/NTooltip';
 export * from './components/Types';
-
-// Export ui components that are used by other components
-export * from './components/ui/accordion';
-export * from './components/ui/alert';
-export * from './components/ui/alert-dialog';
-export * from './components/ui/badge';
-export * from './components/ui/button';
-export * from './components/ui/checkbox';
-export * from './components/ui/dialog';
-export * from './components/ui/dropdown-menu';
-export * from './components/ui/input';
-export * from './components/ui/label';
-export * from './components/ui/popover';
-export * from './components/ui/progress';
-export * from './components/ui/radio-group';
-export * from './components/ui/separator';
-export * from './components/ui/sheet';
-export * from './components/ui/skeleton';
-export * from './components/ui/slider';
-export * from './components/ui/switch';
-export * from './components/ui/table';
-export * from './components/ui/tabs';
-export * from './components/ui/textarea';
-export * from './components/ui/toast';
-export * from './components/ui/toaster';
-export * from './components/ui/tooltip';
-export * from './components/ui/use-toast';
 
 // Export lib utilities
 export * from './lib/utils';
