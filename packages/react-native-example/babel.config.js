e3d85a657a7706a4847e5098f6a714d54b004cdd
ursnj <niranjan.devasani@gmail.com>
2025-07-21 23:59:24 +0530

    Add React Native

---
+++ packages/react-native-example/babel.config.js
@@ -0,0 +1,8 @@
+module.exports = function (api) {
+  api.cache(true);
+  let plugins = [];
+  return {
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    plugins,
+  };
+};
