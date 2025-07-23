4ac599a1cb9a05663b6a376530b193e9ae785dd0
ursnj <niranjan.devasani@gmail.com>
2025-07-23 23:00:58 +0530

    Improve Components

--- packages/react-native/src/components/NCard.tsx
+++ packages/react-native/src/components/NCard.tsx
@@ -1,12 +1,18 @@
+import React from 'react';
 import { View, type ViewProps } from 'react-native';
 import { cn } from '@/lib/utils';
 
-export const NCard = (props: ViewProps) => {
+export interface NCardProps extends ViewProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export const NCard = React.memo<NCardProps>(({ children, className, ...props }) => {
   return (
-    <View
-      className={cn('bg-card border border-border rounded', props.className)}
-    >
-      {props.children}
+    <View className={cn('bg-card border border-border rounded-lg p-4', className)} {...props}>
+      {children}
     </View>
   );
-};
+});
+
+NCard.displayName = 'NCard';
