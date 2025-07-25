51dc789b98db5d9eb6adf28853c62521fa6a61a0
ursnj <niranjan.devasani@gmail.com>
2025-07-25 20:23:44 +0530

    Update Website

--- packages/website/src/services/CliAttributes.ts
+++ packages/website/src/services/CliAttributes.ts
@@ -1,49 +1,49 @@
 export const sitemapsCreateAttributes = [
-  { name: 'Website URL', attribute: '--website / -w', type: 'string', default: '', details: 'Pass website base URL to start crawling.' },
+  { name: 'Website URL', attribute: '--website / -w', type: 'string', default: '', details: 'The URL of the website to crawl.' },
   {
     name: 'Replacement Website',
     attribute: '--replacer / -r',
     type: 'string',
     default: '',
-    details: 'Pass replacement URL to replace crawled url, this will be mostly useful to crawl localhost and replace it with original URL..'
+    details: 'The URL of the website to be replaced. This will be mostly useful to crawl localhost and replace it with original URL.'
   },
   {
     name: 'Crawling depth',
     attribute: '--depth / -d',
     type: 'number',
     default: '10',
-    details: 'Pass depth to let the generator know how depth it need to crawl.'
+    details: 'Depth of the website to crawl.'
   },
   {
     name: 'Change frequency',
     attribute: '--changefreq / -f',
     type: 'string',
     default: 'daily',
-    details:
-      'Pass change frequency to let the generator know how frequently your content change, possible options are always, hourly, daily, weekly, monthly, yearly, never.'
+    details: 'Change frequency for the sitemap. Possible options are: always, hourly, daily, weekly, monthly, yearly, never.'
   },
   {
     name: 'Output',
     attribute: '--output / -o',
     type: 'string',
     default: './sitemap.xml',
-    details: 'Pass output to let the generator know where to keep generated sitemap.'
+    details: 'Output path for the sitemap.xml.'
   }
 ];
+
 export const sitemapsValidationAttributes = [
   {
     name: 'Input sitemap',
     attribute: '--input / -i',
     type: 'string',
     default: './sitemap.xml',
-    details: 'Pass input sitemap to let the generator know where to find and validate sitemap.'
+    details: 'Input path for the sitemap.xml.'
   },
   {
     name: 'Is Remote',
     attribute: '--isremote / -ir',
     type: 'boolean',
     default: 'false',
-    details: 'Pass isremote true / false to the tool to know if its a local file or remote URL.'
+    details: 'Pass true if sitemap is hosted somewhere.'
   }
 ];
 
@@ -53,37 +53,38 @@ export const robotsCreateAttributes = [
     attribute: '--allowed / -a',
     type: 'string',
     default: '',
-    details: 'Pass allowed links to allow search engines to crawling.'
+    details: 'Allowed paths for the robots.txt.'
   },
   {
     name: 'Disallowed paths',
     attribute: '--disallowed / -d',
     type: 'string',
     default: '',
-    details: 'Pass dis allowed links to restrict search engines from crawling.'
+    details: 'Disallowed paths for the robots.txt.'
   },
-  { name: 'Sitemap', attribute: '--sitemap / -s', type: 'string', default: '', details: 'Pass site map URL of your website.' },
+  { name: 'Sitemap', attribute: '--sitemap / -s', type: 'string', default: '', details: 'Sitemap url for the robots.txt.' },
   {
     name: 'Output',
     attribute: '--output / -o',
     type: 'string',
-    default: './sitemap.xml',
-    details: 'Pass output to let the generator know where to keep generated sitemap.'
+    default: './robots.txt',
+    details: 'Output path for the robots.txt.'
   }
 ];
+
 export const robotsValidationAttributes = [
   {
     name: 'Input Robots.txt',
     attribute: '--input / -i',
     type: 'string',
     default: './robots.txt',
-    details: 'Pass input robots.txt to let the generator know where to find and validate robots.txt.'
+    details: 'Input path for the robots.txt.'
   },
   {
     name: 'Is Remote',
     attribute: '--isremote / -ir',
     type: 'boolean',
     default: 'false',
-    details: 'Pass isremote true / false to the tool to know if its a local file or remote URL.'
+    details: 'Pass true if robots.txt is hosted somewhere.'
   }
 ];
