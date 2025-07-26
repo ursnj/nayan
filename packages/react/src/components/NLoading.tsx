3174ee1c1a694ea142961660e6b120fd4f4c6b9c
ursnj <niranjan.devasani@gmail.com>
2025-07-26 13:20:01 +0530

    Update Website

--- packages/react/src/components/NLoading.tsx
+++ packages/react/src/components/NLoading.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React from 'react';
 import { cn } from '../lib/utils';
 
 export interface NLoadingProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -6,25 +6,22 @@ export interface NLoadingProps extends React.HTMLAttributes<HTMLDivElement> {
   'aria-label'?: string;
 }
 
-export const NLoading = React.memo(
-  forwardRef<HTMLDivElement, NLoadingProps>(({ className = '', 'aria-label': ariaLabel = 'Loading', ...rest }, ref) => {
-    return (
-      <div className="p-3">
-        <div
-          ref={ref}
-          role="status"
-          aria-live="polite"
-          aria-label={ariaLabel}
-          className={cn(
-            'animation-loader relative text-slate-400 text-[5px] h-[1em] w-[1em] my-[20px] mx-auto -indent-[999em] rounded-full',
-            className
-          )}
-          {...rest}>
-          Loading...
-        </div>
+export const NLoading = React.memo(({ className = '', 'aria-label': ariaLabel = 'Loading', ...rest }: NLoadingProps) => {
+  return (
+    <div className="p-3">
+      <div
+        role="status"
+        aria-live="polite"
+        aria-label={ariaLabel}
+        className={cn(
+          'animation-loader relative text-slate-400 text-[5px] h-[1em] w-[1em] my-[20px] mx-auto -indent-[999em] rounded-full',
+          className
+        )}
+        {...rest}>
+        Loading...
       </div>
-    );
-  })
-);
+    </div>
+  );
+});
 
 NLoading.displayName = 'NLoading';
